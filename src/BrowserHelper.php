11799424d5437cec315c71812bb8751e6d4a8b8d
Aad Pouw <aad.pouw@gmail.com>
2020-05-03 18:39:36 +0200

    Add files via upload
    What has improved is the Safari part, this is looking now to the 'Safari/xxx.xx.x' at the end of the user agent string.
    Also improved is the 'getBrowserInfo' function, 'Version' has become 'Browser Version'.

--- src/BrowserHelper.php
+++ src/BrowserHelper.php
@@ -14,7 +14,6 @@
 
 /**
  * Class BrowserHelper
- * @package src\AwdBase\Helpers
  */
 class BrowserHelper
 {
@@ -29,10 +28,14 @@ class BrowserHelper
     const UL_CLOSE = '</ul>';
     const UL_START = '<ul ';
 
+
+
     protected
         $_accept,
         $_attribute,
         $_browser_name,
+        $_browser_version,
+        $_browser_version_comparison,
         $_comparison,
         $_comparisons = array(
         'Equal',
@@ -56,8 +59,6 @@ class BrowserHelper
         $_use_html = false,
         $_user_agent,
         $_user_browser,
-        $_version,
-        $_version_comparison,
         $_wrapper;
 
     /**
@@ -95,11 +96,10 @@ class BrowserHelper
      */
     private function _get_browser(){
         $this->_browser_name = 'Unknown';
+        $this->_browser_version = "";
         $this->_platform = 'Unknown';
-        $this->_user_agent =  $_SERVER['HTTP_USER_AGENT'];
-        $this->_version = "";
+        $this->_user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-        // First get the platforms?
         // First get the platforms?
         if (preg_match('/linux|android/i', $this->_user_agent)):
             $this->_platform = 'Android';
@@ -122,45 +122,52 @@ class BrowserHelper
         elseif(preg_match('/Firefox/i',$this->_user_agent)):
             $this->_browser_name = 'Mozilla Firefox';
             $this->_user_browser = "Firefox";
+        elseif(preg_match('/FxiOS/i',$this->_user_agent)):
+            $this->_browser_name = 'Mozilla FxiOS (Firefox on iPad/iPhone)';
+            $this->_user_browser = "FxiOS";
+        elseif(preg_match('/Edge/i',$this->_user_agent)):
+            $this->_browser_name = 'Ms Edge';
+            $this->_user_browser = "Edge";
         elseif(preg_match('/Chrome/i',$this->_user_agent)):
             $this->_browser_name = 'Google Chrome';
             $this->_user_browser = "Chrome";
         elseif(preg_match('/criOS/i',$this->_user_agent)):
-            $this->_browser_name = 'Google CriOS (Chrome on Safari)';
+            $this->_browser_name = 'Google CriOS (Chrome on iPad/iPhone)';
             $this->_user_browser = "CriOS";
-        elseif(preg_match('/Safari/i',$this->_user_agent)):
-            $this->_browser_name = 'Apple Safari';
-            $this->_user_browser = "Safari";
         elseif(preg_match('/Opera/i',$this->_user_agent)):
             $this->_browser_name = 'Opera';
             $this->_user_browser = "Opera";
+        elseif(preg_match('/Safari/i',$this->_user_agent)): // or Safari?
+            $this->_browser_name = 'Apple Safari';
+            $this->_user_browser = "Safari";
+
         endif;
 
         // finally get the correct version number
-        $this->_known = array('Version', $this->_user_browser, 'other');
+        $this->_known = array('BrowserVersion', $this->_user_browser, 'other');
         $this->_pattern = '#(?<browser>' . join('|', $this->_known) . ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
         if (!preg_match_all($this->_pattern, $this->_user_agent, $this->_matches));
 
         $this->_i = count($this->_matches['browser']);
         if ($this->_i != 1):
-            if (strripos($this->_user_agent,"Version") < strripos($this->_user_agent,$this->_user_browser)):
-                $this->_version= $this->_matches['version'][0];
+            if (strripos($this->_user_agent,"BrowserVersion") < strripos($this->_user_agent,$this->_user_browser)):
+                $this->_browser_version= $this->_matches['version'][0];
             else:
-                $this->_version= $this->_matches['version'][1];
+                $this->_browser_version= $this->_matches['version'][1];
             endif;
         else:
-            $this->_version= $this->_matches['version'][0];
+            $this->_browser_version= $this->_matches['version'][0];
         endif;
 
-        if ($this->_version==null || $this->_version=="")$this->_version="?";
+        if ($this->_browser_version==null || $this->_browser_version=="")$this->_browser_version="?";
 
         return array(
             'userAgent' => $this->_user_agent,
-            'browser_name'  => $this->_browser_name,
-            'user_browser'  => $this->_user_browser,
-            'version'       => $this->_version,
-            'platform'      => $this->_platform,
-            'pattern'       => $this->_pattern
+            'browser_name'      => $this->_browser_name,
+            'browser_version'   => $this->_browser_version,
+            'user_browser'      => $this->_user_browser,
+            'platform'          => $this->_platform,
+            'pattern'           => $this->_pattern
         );
     }
 
@@ -191,7 +198,7 @@ class BrowserHelper
                 $this->_html .= self::LI_START .'class="browser-detail-li"' . self::EL_END . 'Platform: ' . self::SPAN_START . $this->_get_browser()['platform'] . self::SPAN_CLOSE . self::LI_CLOSE;
                 $this->_html .= self::LI_START .'class="browser-detail-li"' . self::EL_END . 'Browser\'s Name: ' . self::SPAN_START . $this->_get_browser()['browser_name'] . self::SPAN_CLOSE . self::LI_CLOSE;
                 $this->_html .= self::LI_START .'class="browser-detail-li"' . self::EL_END . 'User\'s Browser: ' . self::SPAN_START . $this->_get_browser()['user_browser'] . self::SPAN_CLOSE . self::LI_CLOSE;
-                $this->_html .= self::LI_START .'class="browser-detail-li"' . self::EL_END . 'Version: ' . self::SPAN_START . $this->_get_browser()['version'] . self::SPAN_CLOSE . self::LI_CLOSE;
+                $this->_html .= self::LI_START .'class="browser-detail-li"' . self::EL_END . 'Browser Version: ' . self::SPAN_START . $this->_get_browser()['browser_version'] . self::SPAN_CLOSE . self::LI_CLOSE;
                 $this->_html .= self::LI_START .'class="browser-detail-li"' . self::EL_END . 'Pattern: ' . self::SPAN_START . $this->_get_browser()['pattern'] . self::SPAN_CLOSE . self::LI_CLOSE;
             endif;
             $this->_html .= self::UL_CLOSE;
@@ -205,19 +212,6 @@ class BrowserHelper
         endif;
         return $this->_html;
     }
-
-
-    /**
-     * @param $_is_platform
-     * @return bool
-     */
-    public function IsPlatform($_is_platform){
-        $this->_is_platform = $_is_platform;
-        if($this->_is_platform === $this->_get_browser()['platform'] )
-            $this->_return = true;
-        return $this->_return;
-    }
-
 
     /**
      * @param $_is_browser
@@ -239,6 +233,24 @@ class BrowserHelper
     }
 
     /**
+     * @param $_is_platform
+     * @return bool
+     */
+    public function IsPlatform($_is_platform){
+        $this->_is_platform = $_is_platform;
+        if($this->_is_platform === $this->_get_browser()['platform'] )
+            $this->_return = true;
+        return $this->_return;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsPlatform(){
+        return $this->_return;
+    }
+
+    /**
      * @param $_comparison
      * @param $is_version
      * @return bool
@@ -247,46 +259,47 @@ class BrowserHelper
         $this->_comparison = $_comparison;
         $this->_is_version = $is_version;
         if($this->getIsUserBrowser() === true):
-            if($this->_comparisons[0] === $this->_comparison):
-                if($this->_get_browser()['version'] == $this->_is_version):
-                    $this->_version_comparison = true;
+            //if($this->_comparisons[0] === $this->_comparison):
+            if($this->_comparisons[0] === $this->_comparison):    //preg_match()
+                if($this->_get_browser()['browser_version'] == $this->_is_version):
+                    $this->_browser_version_comparison = true;
                 else:
-                    $this->_version_comparison = false;
+                    $this->_browser_version_comparison = false;
                 endif;
             elseif($this->_comparisons[1] === $this->_comparison):
-                if($this->_get_browser()['version'] != $this->_is_version):
-                    $this->_version_comparison = true;
+                if($this->_get_browser()['browser_version'] != $this->_is_version):
+                    $this->_browser_version_comparison = true;
                 else:
-                    $this->_version_comparison = false;
+                    $this->_browser_version_comparison = false;
                 endif;
             elseif($this->_comparisons[2] === $this->_comparison):
-                if($this->_get_browser()['version'] > $this->_is_version):
-                    $this->_version_comparison = true;
+                if($this->_get_browser()['browser_version'] > $this->_is_version):
+                    $this->_browser_version_comparison = true;
                 else:
-                    $this->_version_comparison = false;
+                    $this->_browser_version_comparison = false;
                 endif;
             elseif($this->_comparisons[3] === $this->_comparison):
-                if($this->_get_browser()['version'] < $this->_is_version):
-                    $this->_version_comparison = true;
+                if($this->_get_browser()['browser_version'] < $this->_is_version):
+                    $this->_browser_version_comparison = true;
                 else:
-                    $this->_version_comparison = false;
+                    $this->_browser_version_comparison = false;
                 endif;
             elseif($this->_comparisons[4] === $this->_comparison):
-                if($this->_get_browser()['version'] >= $this->_is_version):
-                    $this->_version_comparison = true;
+                if($this->_get_browser()['browser_version'] >= $this->_is_version):
+                    $this->_browser_version_comparison = true;
                 else:
-                    $this->_version_comparison = false;
+                    $this->_browser_version_comparison = false;
                 endif;
             elseif($this->_comparisons[5] === $this->_comparison):
-                if($this->_get_browser()['version'] <= $this->_is_version):
-                    $this->_version_comparison = true;
+                if($this->_get_browser()['browser_version'] <= $this->_is_version):
+                    $this->_browser_version_comparison = true;
                 else:
-                    $this->_version_comparison = false;
+                    $this->_browser_version_comparison = false;
                 endif;
             else:
-                $this->_version_comparison = false;
+                $this->_browser_version_comparison = false;
             endif;
         endif;
-        return $this->_version_comparison;
+        return $this->_browser_version_comparison;
     }
 }
